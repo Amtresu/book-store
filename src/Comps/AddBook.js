4ce0c40d511150cb695d0efad6ae70576979ae68
Amtresu <conor.follette@gmail.com>
2022-09-23 14:19:50 +0900

    added styling modules and styled the project

--- src/Comps/AddBook.js
+++ src/Comps/AddBook.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/book';
+import './style/addbook.css'
 
 const AddBookForm = () => {
   const dispatch = useDispatch();
@@ -12,12 +13,15 @@ const AddBookForm = () => {
     const author = document.querySelector('.author').value;
     dispatch(addBook(id, title, author));
   };
+
+  
   return (
-    <div>
+    <div className='form-div'>
+      <div className='divider'></div>
       <form>
         <input type="text" className="title" placeholder="Title" />
         <input type="text" className="author" placeholder="Author" />
-        <button onClick={handleEvent} type="submit">Add book</button>
+        <button onClick={handleEvent} type="submit" className='add-book-button'>Add book</button>
       </form>
     </div>
   );
